22d599aec98560b337ef9b05bfc8f183fb1fd1e1
Fernando <90964651+FerPellicer@users.noreply.github.com>
2022-03-28 00:21:07 +0100

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HistoriaMedicaComponent } from './historia-medica/historia-medica.component';
+import { AddHistoriaComponent } from './add-historia/add-historia.component';
+import { AddEmpleadosComponent } from './add-empleados/add-empleados.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'historia', component: HistoriaMedicaComponent, pathMatch: 'full' },
+  { path: 'addhistoria', component: AddHistoriaComponent, pathMatch: 'full' },
+  { path: 'addempleados', component: AddEmpleadosComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
