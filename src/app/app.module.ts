3d43a83b2f5a961a3eff586cf5d673a8e532717b
ACostaCaz <mankydev@gmail.com>
2022-04-18 13:52:55 +0100

    Added login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { RegisterComponent } from './register/register.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
 import { FotoComponent } from './foto/foto.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegisterComponent, EmpleadosComponent, EmpleadoComponent, FotoComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [CookieService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { UsersService } from '../users/users.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  email!: string;
+  password!: string;
 
-  constructor() { }
+  constructor(public userService: UsersService, public router: Router) {}
 
-  ngOnInit(): void {
+  login() {
+    const user = { email: this.email, password: this.password };
+    this.userService.login(user).subscribe(data => {
+      this.userService.setToken(data.token);
+      this.router.navigateByUrl('/');
+    });
   }
-
 }
